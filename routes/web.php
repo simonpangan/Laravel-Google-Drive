5864efda3a82a91f2e05dc47f9ee2d11acd4501f
Simon Pangan <simonjoseph.pangan@benilde.edu.ph>
2021-03-20 01:36:01 +0800

    UI AND PROVIDERS

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/upload', function (Request $request) {
+   dd($request->file("thing"));
 });
